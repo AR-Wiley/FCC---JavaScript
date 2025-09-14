e1ee7fc12a40cf96d0bb0ec1805736003fa7cc38
AR Wiley <98679479+AR-Wiley@users.noreply.github.com>
2025-09-14 16:28:33 -0500

    Add files via upload

---
+++ Variables and Strings/Build a JavaScript Trivia Bot.js
@@ -0,0 +1,23 @@
+console.log("Hello! I'm your coding fun fact guide!")
+
+let botName = "Homer Simpson"
+let botLocation = "Springfield"
+let favoriteLanguage = "JavaScript"
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".")
+
+console.log("My favorite programming language is " + favoriteLanguage + ".")
+
+let codingFact = "Homer is not a programmger but he loves " + favoriteLanguage + "!"
+
+console.log(codingFact)
+
+codingFact = "Homer like " + favoriteLanguage + "more than he likes donuts!"
+
+console.log(codingFact)
+
+codingFact = "But he does not like " + favoriteLanguage + " as much as he likes donuts with sprikles!"
+
+console.log(codingFact)
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".")
---
+++ Variables and Strings/Build a Sentence Maker.js
@@ -0,0 +1,27 @@
+
+let adjective = "happy"
+let noun = "dog"
+let verb = "sneezed"
+let place = "house"
+let adjective2 = "snotty"
+let noun2 = "dounuts"
+
+let firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + "." + " The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + "."
+
+console.log("First story: " + firstStory)
+
+adjective = "estatic"
+noun = "hound"
+verb = "coughed"
+place = "home"
+adjective2 = "burning"
+noun2 = "treats"
+
+let secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + "." + " The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + "."
+
+console.log()
+
+console.log("Second story: " + secondStory)
+
+
+
