9681d0f4cdfb89ebdcb43251209c3a797593996d
AR Wiley <98679479+AR-Wiley@users.noreply.github.com>
2025-12-09 12:19:46 -0600

    Add files via upload

---
+++ Loops/Implement the Mutations Algorithm.js
@@ -0,0 +1,13 @@
+let mutation = (arr) => {
+
+  let str1 = arr[0].toLowerCase()
+  let str2 = arr[1].toLowerCase()
+
+  for (let char of str2) {
+    if (!str1.includes(char)) {
+      return false   
+    }
+  }
+  
+  return true       
+}
